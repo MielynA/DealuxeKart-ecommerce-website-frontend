867c5970163844efb8343527500e7abc3eaba37d
Mielyn Acosta <mielynacosta@pursuit.org>
2019-03-29 11:28:42 -0400

    added signin,sinup and logout and Auth

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import '../styles/footer.css';
 const Footer = (props) => {
     return (
         <React.Fragment>
-            <footer className='container-fluid bg-4 text-center footer'>
+            <footer className='container-fluid bg-4 text-center footer '>
                 <p><h3 className ='mt-5'>Chat with Me</h3></p>
                 <img src='https://image.flaticon.com/icons/svg/234/234149.svg' alt='Chat icon' className='icon'/>
 
@@ -13,31 +13,23 @@ const Footer = (props) => {
                  <input type='email' className='form-control' size='50' placeholder='Email Address' />
                     <button type='button' className='btn btn-danger'>Sign Up</button>
                 </form>
-                <a href='https://twitter.com/' target='_blank' class='share-btn twitter'>
+                <a href='https://twitter.com/' target='blank' className='share-btn twitter mb-3'>
                     <i className='fa fa-twitter'></i>
                 </a>
-                <a href='https://facebook.com/' target='_blank' className='share-btn facebook'>
+                <a href='https://facebook.com/' target='blank' className='share-btn facebook'>
                     <i className='fa fa-facebook'></i>
                 </a>
-                <a href='https://github.com/MielynA' target='_blank' className='share-btn github'>
+                <a href='https://github.com/MielynA' target='blank' className='share-btn github'>
                     <i className='fa fa-github'></i>
                 </a>
-                <a href='https://www.linkedin.com/in/mielyn-de-lara-acosta-4a5587116/' target='_blank' className='share-btn linkedin'>
+                <a href='https://www.linkedin.com/in/mielyn-de-lara-acosta-4a5587116/' target='blank' className='share-btn linkedin'>
                     <i className='fa fa-linkedin'></i>
                 </a>
-                <a href='https://www.instagram.com/mie_shiobe/' target='_blank' className='share-btn instagram'>
+                <a href='https://www.instagram.com/mie_shiobe/' target='blank' className='share-btn instagram'>
                     <i className='fa fa-instagram'></i>
                 </a>
                 
             </footer>
-
-
-
-
-
-
-
-
         </React.Fragment>
 
 
